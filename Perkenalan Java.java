16ef611b70a9894efc9be2e7bb0e19dfbf531f34
Junaaa10 <arjuna_dewanta_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 19:59:24 +0700

    Add files via upload

---
+++ Perkenalan Java.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Nama : Arjuna Prasetya D");
+        System.out.println("Tempat Tanggal Lahir : Malang 10 Juli 2009");
+        System.out.println("Alamat : Perum Pondok Mutiara Kebonagung Rt 20 Rw 06");
+        System.out.println("No Telp : 087720407158");
+        System.out.println("Hobi : Bermain Bola, Bermain Game");
+
+
+
+
+    }
+}
